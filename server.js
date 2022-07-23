5e9471c49b41cc9cadd5ba032f2cdd4b2b37b775
szczechurek <r.szczechura.01@gmail.com>
2022-07-23 20:28:28 +0200

    add about, store page, add styles, add logo, refactor server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const port = 8080; // you can use any port
+const PORT = 8080; // you can use any port
 const app = express();
 app.use(express.static(__dirname + '/public'));
-app.listen(port);
-console.log('server on' + port);
+app.listen(PORT, () =>
+  console.log(`Server running on: http://localhost:${PORT}`)
+);
